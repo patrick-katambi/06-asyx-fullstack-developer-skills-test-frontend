d2c13314eae97eddf848994ec513e7c982bea01d
patrick-katambi <katambipatrick@gmail.com>
2022-04-07 06:37:11 +0100

    setting up url config

---
+++ src/core/urls.js
@@ -0,0 +1,9 @@
+const baseUrl = 'http://localhost:8000'
+const baseApiUrl = `${baseUrl}/api`
+const userApiUrl = `${baseApiUrl}/user`
+
+export const urls = {
+    user: {
+        register: `${userApiUrl}/register`
+    }
+}
